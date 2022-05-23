34b0f2c08ff8198f239aa23a7877cd9057fb85e9
wantingmao01 <97856917+wantingmao01@users.noreply.github.com>
2022-05-22 23:32:20 -0700

    Update chi_squared_identity_tester.py

--- chi_squared_identity_tester.py
+++ chi_squared_identity_tester.py
@@ -7,26 +7,26 @@ Parameters:
 n: total number of bins in distribution, {100, 1000, 10000}
 epsilon: deviation, a double-float number between [0.1, 0.5]
 q: weight of the known distribution, expected to be a list of float numbers,
-    that the sum of the list is 1
+    that length is n and sum of the list is 1
 S: samples taken from the distribution, expected to be a list of integers
     that ranges from 0 to (n-1)
 """
-def chi_squared(n, epsilon, q, S):
+def chi_squared(n, epsilon, q, s):
   c_m = 1.45
   c_t = 0.382
   m = int(c_m*(n**0.5)/(epsilon**2))
   if len(S) < m:
     return "must be at least " +str(m)+" samples"
+  if n != len(q):
+    return "lengh of q must match n"
 
-  n = len(q)
   a = [i for i in range(n) if q[i] >= epsilon/(50*n)]
 
   #generate Ni for i in a
   N = {}
   for i in a:
     N[i] = 0
-  for elem in SAMPp:
-    i = elem
+  for i in s:
     if i not in a:
       continue
     if i not in N:
