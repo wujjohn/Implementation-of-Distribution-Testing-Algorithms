f2e85b054cc82fe0f20404f91cde13e7430cb5eb
wantingmao01 <97856917+wantingmao01@users.noreply.github.com>
2022-05-22 23:27:01 -0700

    Create chi_squared_identity_tester.py

---
+++ chi_squared_identity_tester.py
@@ -0,0 +1,43 @@
+"""
+This funtion implements chi-squared tester for identity testing.
+https://arxiv.org/pdf/1507.05952.pdf
+
+
+Parameters:
+n: total number of bins in distribution, {100, 1000, 10000}
+epsilon: deviation, a double-float number between [0.1, 0.5]
+q: weight of the known distribution, expected to be a list of float numbers,
+    that the sum of the list is 1
+S: samples taken from the distribution, expected to be a list of integers
+    that ranges from 0 to (n-1)
+"""
+def chi_squared(n, epsilon, q, S):
+  c_m = 1.45
+  c_t = 0.382
+  m = int(c_m*(n**0.5)/(epsilon**2))
+  if len(S) < m:
+    return "must be at least " +str(m)+" samples"
+
+  n = len(q)
+  a = [i for i in range(n) if q[i] >= epsilon/(50*n)]
+
+  #generate Ni for i in a
+  N = {}
+  for i in a:
+    N[i] = 0
+  for elem in SAMPp:
+    i = elem
+    if i not in a:
+      continue
+    if i not in N:
+      N[i] = 1
+    else:
+      N[i] += 1
+
+  #calculate Z
+  Z = sum([((N[i]-m*q[i])**2-N[i])/(m*q[i]) for i in a])
+
+  if Z <= c_t*m*(epsilon**2):
+    return True
+  else:
+    return False
